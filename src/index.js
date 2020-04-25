d04dc380bc749f8f432b0dcd58994f424c3f5c31
DivyanshRai27 <divyanshrai27@gmail.com>
2020-04-25 16:43:11 +0530

    read tasks

--- src/index.js
+++ src/index.js
@@ -50,6 +50,28 @@ app.post('/tasks', (req, res) => {
     })
  })
 
+ app.get('/tasks', (req, res) => {
+    Task.find({}).then((task) => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(500)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+         if (!task) {
+             return res.status(404).send('')
+         } 
+         res.send(task)
+    }).catch((e) => {
+        res.status(400).send()
+    })
+
+})
+
 app.listen(port, () => {
     console.log('Server is on port ' + port)
 })
